7b31ac3a03033ee4740b0106b8641212c9a69e99
tgogo63 <tgogo63@gmail.com>
2021-04-19 13:21:17 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # spring-boot-api-example
+# spring-boot-api-example
---
+++ src/main/java/com/kwtomjr/ThemeParkApplication.java
@@ -0,0 +1,25 @@
+package com.kwtomjr;
+
+import com.kwtomjr.entity.ThemeParkRide;
+import com.kwtomjr.repository.ThemeParkRideRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ThemeParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeParkApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+		return (args) -> {
+			repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+			repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+			repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+		};
+	}
+
+}
